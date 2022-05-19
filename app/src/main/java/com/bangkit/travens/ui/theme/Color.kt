741f73c7e1ad2b3ccef37c311b6151b561e9a20d
Setyo Fajar <setyofajar@gmail.com>
2022-05-19 15:51:54 +0700

    feat: add top app bar

--- app/src/main/java/com/bangkit/travens/ui/theme/Color.kt
+++ app/src/main/java/com/bangkit/travens/ui/theme/Color.kt
@@ -6,6 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val White = Color(0xFFFFFFFF)
 
 val Primary = Color(0xFF2D8CFF)
 val Secondary = Color(0xFFFF8B00)
