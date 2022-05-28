17a76f1b814af78f7a901f8c55dd086aadff1e06
widi-ps <widyapujisaputro@gmail.com>
2022-05-28 08:11:55 +0700

    feat: login and signup

--- app/src/main/java/com/bangkit/travens/ui/login/LoginActivity.kt
+++ app/src/main/java/com/bangkit/travens/ui/login/LoginActivity.kt
@@ -3,10 +3,20 @@ package com.bangkit.travens.ui.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bangkit.travens.R
+import com.bangkit.travens.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
 
 class LoginActivity : AppCompatActivity() {
+
+	private lateinit var binding: ActivityLoginBinding
+	private lateinit var firebaseAuth: FirebaseAuth
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_login)
+		binding = ActivityLoginBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 	}
 }
