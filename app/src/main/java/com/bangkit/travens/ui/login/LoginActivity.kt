f7e6bad845b1e1dd1dd9710ab06359e4d65dccb0
widi-ps <widyapujisaputro@gmail.com>
2022-05-28 13:03:12 +0700

    feat: login & register setup

--- app/src/main/java/com/bangkit/travens/ui/login/LoginActivity.kt
+++ app/src/main/java/com/bangkit/travens/ui/login/LoginActivity.kt
@@ -1,9 +1,14 @@
 package com.bangkit.travens.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.bangkit.travens.MainActivity
 import com.bangkit.travens.R
 import com.bangkit.travens.databinding.ActivityLoginBinding
+import com.bangkit.travens.ui.signup.SignUpActivity
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -18,5 +23,40 @@ class LoginActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		binding = ActivityLoginBinding.inflate(layoutInflater)
 		setContentView(binding.root)
+
+		firebaseAuth = FirebaseAuth.getInstance()
+		binding.btnRegister.setOnClickListener{
+			val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+			startActivity(intent)
+		}
+
+		binding.btnLogin.setOnClickListener{
+			val email = binding.emailEt.text.toString()
+			val password = binding.passET.text.toString()
+
+			if(email.isNotEmpty() && password.isNotEmpty()){
+				firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+					if(it.isSuccessful){
+						val intent = Intent(this, MainActivity::class.java)
+						startActivity(intent)
+					} else {
+						Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+					}
+				}
+			} else {
+				Toast.makeText(this, "Field tidak boleh kosong !!", Toast.LENGTH_SHORT).show()
+			}
+		}
 	}
+
+	override fun onStart() {
+		super.onStart()
+
+		if(firebaseAuth.currentUser != null){
+			val intent = Intent(this,MainActivity::class.java)
+			startActivity(intent)
+			finish()
+		}
+	}
+
 }
